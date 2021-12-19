08b12cbc27bb261f9ae81f35071b0f3348201385
Praneeth <64746260+praneethsiddu@users.noreply.github.com>
2021-12-19 13:21:18 +0530

    Update AandG.py

--- pages/AandG.py
+++ pages/AandG.py
@@ -14,12 +14,12 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 from st_aggrid.shared import GridUpdateMode
 import os
 def app():
-    path = os.getcwd()
-    st.write(path)
-    if 'main_data.csv' not in os.listdir(path+"\data"):
+    #path = os.getcwd()
+    #st.write(path)
+    if 'main_data.csv' not in os.listdir("data"):
         st.markdown("Please upload data through `Upload Data` page!")
     else:
-        data = pd.read_csv(path+'\data\main_data.csv')
+        data = pd.read_csv('data/main_data.csv')
         gb = GridOptionsBuilder.from_dataframe(data)
         gb.configure_pagination()
         gb.configure_side_bar()
